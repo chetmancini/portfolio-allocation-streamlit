18f66aecce72abfe6d940d578cf3d5fdf7dc964a
Chet Mancini <chetmancini@gmail.com>
2023-10-21 14:50:06 -0400

    basic app

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Portfolio Optimization")
+
+st.file_uploader("Upload your portfolio", type="csv")
+
+st.form_submit_button("Submit")
+
+st.write("This is a streamlit ")
+
+st.sidebar.title("Sidebar")
